956c0d2d25147837acd3e4a63c10f90d1fc9141d
JOHN NASIMBA <nasimba4john@gmail.com>
2021-09-29 16:36:07 +0200

    Building the ui for client header

--- resources/views/components/header/client-header.blade.php
+++ resources/views/components/header/client-header.blade.php
@@ -1,16 +1,15 @@
 
 {{-- All CSS Classes for client header begins with "ch" which stands for 'Client Header'. The reason for this is to avoid conficts between css classes as the project grows --}}
 
-<nav class="ch-wrapper">
+<div class="ch-wrapper">
     <div class="ch-amara-capital-logo">
         <img src="/images/amara-capital-images/amara-capital-secondary-logo.png" alt="Amara Capital">
     </div>
     <div class="ch-client-name-container">
-        <h6 class="ch-client-name">Hi, Jane Doe</h6>
-        <figure class="ch-client-profile-image-and-barge-container">
-            <i class="fas fa-bell"></i>
-            <div class="ch-">circle</div>
-        </figure>
+        <h6 class="ch-client-name">Hi, John Nasimba</h6>
+        <div class="ch-client-profile-image-and-barge-container">
+            <div class="ch-client-profile-image"><i class="fas fa-user fa-2x"></i></div>
+        </div>
     </div>
-</nav>
+</div>
     
