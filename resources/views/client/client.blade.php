b326f869fcdbaa135088fa6bd45ac218a4ddf85d
JOHN NASIMBA <nasimba4john@gmail.com>
2021-09-30 09:42:17 +0200

    Creating side nav

--- resources/views/client/client.blade.php
+++ resources/views/client/client.blade.php
@@ -40,7 +40,7 @@
         </div>
         <div class="clp-price-chart-and-side-nav">
            <!-- TradingView Widget BEGIN -->
-            <div id="tradingview_202c3"></div>
+            <div id="tradingview_202c3">
                 <div class="tradingview-widget-container">
                     <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/FX_IDC-XAGZAR/" rel="noopener" target="_blank"><span class="blue-text">XAGZAR Chart</span></a> by TradingView</div>
                     <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
@@ -64,7 +64,43 @@
                     );
                     </script>
                 </div>
+            </div>
             <!-- TradingView Widget END -->
+
+            <div class="clp-side-nav">
+                <h3 class="clp-side-nav-title">GOLD AND SILVER PRICES</h3>
+                <div class="clp-gold-and-silver-price-container">
+                    <div class="clp-gold-price-container">
+                        <h4 class="clp-gold-price">R25,081.73/g</h4>
+                        <p>GOLD(XAU/ZAR)</p>
+                        <button>BUY</button>
+                    </div>
+                    <div class="clp-silver-price-container">
+                        <h4 class="clp-silver-price">R330.73/g</h4>
+                        <p>SILVER(XAG/ZAR)</p>
+                        <button>BUY</button>
+                    </div>
+                </div>
+                <h3 class="clp-client-status">YOUR PERSONAL STATUS</h3>
+                <ul class="clp-client-status-container">
+                    <li class="clp-status">
+                        <h4>R0.00</h4>
+                        <h5>AVAILABLE FUNDS</h5>
+                    </li>
+                    <li class="clp-status">
+                        <h4>201.05</h4>
+                        <h5>POOL UNITS</h5>
+                    </li>
+                    <li class="clp-status">
+                        <h4>R1990.00</h4>
+                        <h5>PROFITS</h5>
+                    </li>
+                    <li class="clp-status">
+                        <h4>98.86%</h4>
+                        <h5>PORTFOLIO GROWTH</h5>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
     
