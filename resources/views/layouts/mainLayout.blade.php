f646060298d86facc8c4c4acac6b95a71d3ff60a
JOHN NASIMBA <nasimba4john@gmail.com>
2021-10-01 12:57:18 +0200

    Fixing media query not working in mobile browser

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Amara Capital</title>
     <link href="/css/app.css" rel="stylesheet">
     <link href="/css/client-header.styles.css" rel="stylesheet">
