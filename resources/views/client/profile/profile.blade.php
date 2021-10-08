994c0166308c615f3e46a13bed427f505e7ca630
JOHN NASIMBA <nasimba4john@gmail.com>
2021-10-08 10:50:41 +0200

    Creating client profile layout

--- resources/views/client/profile/profile.blade.php
+++ resources/views/client/profile/profile.blade.php
@@ -6,6 +6,14 @@
         <div class="cp-name-container">
             <h3>Dashboard</h3>
         </div>
+        <div class="cp-profile-container">
+           <div class="cp-prfile-image-container">
+               profile image
+            </div> 
+            <div class="cp-profile-details-container">
+                profile details
+            </div>
+        </div>
 
     </div>
 @endsection
