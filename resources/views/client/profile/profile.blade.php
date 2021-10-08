ce8bff8ab9f31d2d76e5080f7caa05a0a66e218b
JOHN NASIMBA <nasimba4john@gmail.com>
2021-10-08 10:43:27 +0200

    Creating  client profile header

--- resources/views/client/profile/profile.blade.php
+++ resources/views/client/profile/profile.blade.php
@@ -3,6 +3,9 @@
     @include('components.header.client-header')
     {{-- All class names for client profile begins with "cp" meaning 'client-profile' --}}
     <div class="cp-wrapper">
-        
+        <div class="cp-name-container">
+            <h3>Dashboard</h3>
+        </div>
+
     </div>
 @endsection
