dae22e3bb0f468a9393d5bc5ab936e58cb81d2d9
JOHN NASIMBA <nasimba4john@gmail.com>
2021-10-08 14:50:35 +0200

    Updating client profile image

--- resources/views/client/profile/profile.blade.php
+++ resources/views/client/profile/profile.blade.php
@@ -8,15 +8,15 @@
         </div>
         <div class="cp-profile-container">
            <div class="cp-profile-image-container">
-               <div class="cp-image-and-name-container">
-                   {{-- This will be conditionally renderd. If profile is available, an image will be displayed else this icon --}}
-                   <i class="fas fa-user-circle"></i>
-                   <div class="edit">
+                {{-- This will be conditionally renderd. If profile is available, an image will be displayed else this icon --}}
+                <div class="image-and-edit">
+                    <i class="fas fa-user-circle"></i>    
+                    <div class="edit-container">
                         <i class="fas fa-edit"></i>
-                        <a href="/edit">Edit profile picture</a>
-                   </div>
-                    <h6>John Nasimba</h6>
-               </div>
+                        <a href="/edit">Edit </a>
+                    </div>
+                </div>
+                <h2 class="cp-client-name">John Nasimba</h2>
             </div> 
             <div class="cp-profile-details-container">
                <h4>PERSONAL DETAILS</h4>
