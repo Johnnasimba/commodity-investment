872de1733c0e4dd679fd8964134109aee2ee9b6a
JOHN NASIMBA <nasimba4john@gmail.com>
2021-10-10 11:34:18 +0200

    Creating client details container

--- resources/views/client/profile/profile.blade.php
+++ resources/views/client/profile/profile.blade.php
@@ -22,8 +22,65 @@
                <h4 class="cp-details-title">PERSONAL DETAILS</h4>
                <ul class="personal-details-container">
                    <li class="details-card">
-                       <p class="key">Customer ID</p>
+                       <p class="key">Customer ID:</p>
                        <p class="value">1701001</p>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">First Name:</p>
+                       <p class="value">John</p>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Last Name:</p>
+                       <p class="value">Nasimba</p>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Date of Birth:</p>
+                       <p class="value">1997-08-02</p>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Gander:</p>
+                       <p class="value">Male</p>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Country/Region:</p>
+                       <p class="value">South Africa</p>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Address:</p>
+                       <p class="value">7456 m Mkwayi, Hout bay, Cape Town</p>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Primary Phone Number:</p>
+                       <p class="value">27-80387591</p>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Secondary Phone Number:</p>
+                       <p class="value">27-80387591</p>
+                       <a href="/edit">Edit </a>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Primary Email:</p>
+                       <p class="value">nasimba4john@gmail.com</p>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Secondary Email:</p>
+                       <p class="value">nasimba4john@gmail.com</p>
+                       <a href="/edit">Edit </a>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Estimated Annual Income:</p>
+                       <p class="value">Less than 50,000</p>
+                       <a href="/edit">Edit </a>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Source of Funds:</p>
+                       <p class="value">Savings</p>
+                       <a href="/edit">Edit </a>
+                   </li>
+                   <li class="details-card">
+                       <p class="key">Occupation:</p>
+                       <p class="value">Computer Programmer</p>
+                       <a href="/edit">Edit </a>
                    </li>
                </ul>
             </div>
