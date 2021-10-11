851e4088ab5a7914f0ef0076c95f85bc952cac5a
JOHN NASIMBA <nasimba4john@gmail.com>
2021-10-11 21:49:46 +0200

    Creating declaimer page

--- resources/views/declaimer/declaimer.blade.php
+++ resources/views/declaimer/declaimer.blade.php
@@ -4,8 +4,8 @@
     {{-- All class names for cdivent profile begins with "cp" meaning 'cdivent-profile' --}}
     <div class="declaimer-wrapper">
        <div class="declaimer-container">
-           <h2>DECLAIMER</h2>
-
+           <h2 class="declaimer-title">DECLAIMER</h2>
+           <iframe width="800" height="500" src="https://www.youtube-nocookie.com/embed/b2xmKQTLL9E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
            <a href="/client">skip</a>
        </div>
     </div>
