a0f0c5c1fdce11bab5cab0e0dcd9ccd2a58f8eed
JOHN NASIMBA <nasimba4john@gmail.com>
2021-10-11 21:53:24 +0200

    Updating the length of embeded youtube video

--- resources/views/declaimer/declaimer.blade.php
+++ resources/views/declaimer/declaimer.blade.php
@@ -5,7 +5,7 @@
     <div class="declaimer-wrapper">
        <div class="declaimer-container">
            <h2 class="declaimer-title">DECLAIMER</h2>
-           <iframe width="800" height="500" src="https://www.youtube-nocookie.com/embed/b2xmKQTLL9E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           <iframe width="890" height="500" src="https://www.youtube-nocookie.com/embed/b2xmKQTLL9E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
            <a href="/client">skip</a>
        </div>
     </div>
