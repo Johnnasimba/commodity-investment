ec176f5fc7cab45cd6199a3ed8a05e9866086fa0
JOHN NASIMBA <nasimba4john@gmail.com>
2021-10-14 14:23:17 +0200

    Updating background of register page

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,4 +1,4 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0" style="background-image: linear-gradient(#142F65, #000);">
     <div>
         {{ $logo }}
     </div>
