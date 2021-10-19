0072b7d056dfb178bcc013e424e912885aae7825
JOHN NASIMBA <nasimba4john@gmail.com>
2021-10-19 10:06:49 +0200

    Adding account settings pages to dropdown

--- resources/views/components/header/dropdown/dropdown.blade.php
+++ resources/views/components/header/dropdown/dropdown.blade.php
@@ -2,43 +2,41 @@
 <div class="ch-dropdown-wrapper" id="dropdown">
     <div class="dropdown-container">
         {{-- Link card class is also used with Jquery for hover effect. Do not change this class without modifying Jquery --}}
-        <div class="ch-link-card">
+        <a href="{{ url('client') }}" class="ch-link-card">
             <span class="iconify dropdown-icon" data-icon="ci:dashboard"></span>
-            <a href="{{ url('client') }}">DASHBOARD</a>
-        </div>
-        <div class="ch-link-card">
+            <h3 class="ch-title">DASHBOARD</h3>
+        </a>
+        <a href="{{ url('client/profile') }}" class="ch-link-card">
             <span class="iconify dropdown-icon" data-icon="healthicons:ui-user-profile-outline"></span>
-            <a href="{{ url('client/profile') }}">PROFILE</a>
-        </div>
-        <div class="ch-link-card">
+            <h3 class="ch-title">PROFILE</h3>
+        </a>
+        <a href="{{ url('/client/no-history') }}" class="ch-link-card">
             <span class="iconify dropdown-icon" data-icon="fa-solid:history"></span>
-            <a href="{{ url('/client/no-history') }}">HISTORY</a>
-        </div>
-        <div class="ch-link-card">
+            <h3 class="ch-title">HISTORY</h3>
+        </a>
+        <a href="{{ url('/client/profile/upload-docs') }}" class="ch-link-card">
             <span class="iconify" data-icon="fa-solid:file-upload"></span>
-            <a href="{{ url('/client/profile/upload-docs') }}">UPLOAD DOCS</a>
-        </div>
-        <div class="ch-link-card">
+            <h3 class="ch-title">UPLOAD DOCS</h3>
+        </a>
+        <a href="{{ url('/client/profile/unverified-account') }}" class="ch-link-card">
             <span class="iconify dropdown-icon" data-icon="et:wallet"></span>
-            <a href="{{ url('/client/profile/unverified-account') }}">WALLET</a>
-        </div>
-        <div class="ch-link-card">
+            <h3 class="ch-title">WALLET</h3>
+        </a>
+        <a href="{{ url('/user/profile') }}" class="ch-link-card">
             <span class="iconify" data-icon="carbon:user-settings"></span>
-            <a href="{{ url('/user/profile') }}">ACCOUNT SETTINGS</a>
-        </div>
+            <h3 class="ch-title">ACCOUNT SETTINGS</h3>
+        </a>
         <div class="ch-link-card">
             <span class="iconify dropdown-icon" data-icon="ri:logout-circle-line"></span>
-            <a href="{{ url('client') }}">
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-    
-                    <x-jet-responsive-nav-link href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                    this.closest('form').submit();">
-                        {{ __('LOG OUT') }}
-                    </x-jet-responsive-nav-link>
-                </form>
-            </a>
+            <form method="POST" action="{{ route('logout') }}" class="ch-title">
+                @csrf
+
+                <x-jet-responsive-nav-link style="color:#283048;text-decoration:none;padding-top:10px;" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                this.closest('form').submit();">
+                    {{ __('LOG OUT') }}
+                </x-jet-responsive-nav-link>
+            </form>
         </div>
     </div>        
 </div>
